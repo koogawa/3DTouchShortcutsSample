8af020aea15757802d645f2b388cf60cf5a3a56d
koogawa <koogawa.app@gmail.com>
2015-10-01 22:42:36 +0900

    Add files

---
+++ 3DTouchShortcutsSample/AppDelegate.swift
@@ -0,0 +1,134 @@
+//
+//  AppDelegate.swift
+//  3DTouchShortcutsSample
+//
+//  Created by koogawa on 2015/10/01.
+//  Copyright © 2015年 Kosuke Ogawa. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    // MARK: Types
+
+    enum ShortcutIdentifier: String {
+        case First
+        case Second
+        case Third
+        case Fourth
+
+        // MARK: Initializers
+
+        init?(fullType: String) {
+            guard let last = fullType.componentsSeparatedByString(".").last else { return nil }
+
+            self.init(rawValue: last)
+        }
+
+        // MARK: Properties
+
+        var type: String {
+            return NSBundle.mainBundle().bundleIdentifier! + ".\(self.rawValue)"
+        }
+    }
+
+    // MARK: Static Properties
+
+    static let applicationShortcutUserInfoIconKey = "applicationShortcutUserInfoIconKey"
+
+    // MARK: Properties
+
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+
+        // Override point for customization after application launch.
+        if let shortcutItems = application.shortcutItems where shortcutItems.isEmpty {
+            // Construct the items.
+            let shortcut3 = UIMutableApplicationShortcutItem(type: "ShortcutIdentifier.Third.type", localizedTitle: "Play", localizedSubtitle: "Will Play an item", icon: UIApplicationShortcutIcon(type: .Play), userInfo: [
+                AppDelegate.applicationShortcutUserInfoIconKey: UIApplicationShortcutIconType.Play.rawValue
+                ]
+            )
+
+            let shortcut4 = UIMutableApplicationShortcutItem(type: "ShortcutIdentifier.Fourth.type", localizedTitle: "Pause", localizedSubtitle: "Will Pause an item", icon: UIApplicationShortcutIcon(type: .Pause), userInfo: [
+                AppDelegate.applicationShortcutUserInfoIconKey: UIApplicationShortcutIconType.Pause.rawValue
+                ]
+            )
+
+            // Update the application providing the initial 'dynamic' shortcut items.
+            application.shortcutItems = [shortcut3, shortcut4]
+        }
+
+        return true
+    }
+
+    func applicationWillResignActive(application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    }
+
+    func applicationDidEnterBackground(application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+
+    func applicationWillEnterForeground(application: UIApplication) {
+        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    }
+
+    func applicationDidBecomeActive(application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+    func applicationWillTerminate(application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+
+    func application(application: UIApplication, performActionForShortcutItem shortcutItem: UIApplicationShortcutItem, completionHandler: Bool -> Void) {
+        let handledShortCutItem = handleShortCutItem(shortcutItem)
+        completionHandler(handledShortCutItem)
+    }
+
+    func handleShortCutItem(shortcutItem: UIApplicationShortcutItem) -> Bool {
+        var handled = false
+
+        // Verify that the provided `shortcutItem`'s `type` is one handled by the application.
+        guard ShortcutIdentifier(fullType: shortcutItem.type) != nil else { return false }
+
+        guard let shortCutType = shortcutItem.type as String? else { return false }
+
+        switch (shortCutType) {
+        case ShortcutIdentifier.First.type:
+            // Handle shortcut 1 (static).
+            handled = true
+            break
+        case ShortcutIdentifier.Second.type:
+            // Handle shortcut 2 (static).
+            handled = true
+            break
+        case ShortcutIdentifier.Third.type:
+            // Handle shortcut 3 (dynamic).
+            handled = true
+            break
+        case ShortcutIdentifier.Fourth.type:
+            // Handle shortcut 4 (dynamic).
+            handled = true
+            break
+        default:
+            break
+        }
+
+        // Construct an alert using the details of the shortcut used to open the application.
+        let alertController = UIAlertController(title: "Shortcut Handled", message: "\"\(shortcutItem.localizedTitle)\"", preferredStyle: .Alert)
+        let okAction = UIAlertAction(title: "OK", style: .Default, handler: nil)
+        alertController.addAction(okAction)
+
+        // Display an alert indicating the shortcut selected from the home screen.
+        window!.rootViewController?.presentViewController(alertController, animated: true, completion: nil)
+
+        return handled
+    }
+}
+
---
+++ 3DTouchShortcutsSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  3DTouchShortcutsSample
+//
+//  Created by koogawa on 2015/10/01.
+//  Copyright © 2015年 Kosuke Ogawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
